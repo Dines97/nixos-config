ff676f93fd7bb0fe615a2c10849891cb04cfab0f
Denis Kaynar <kaynar.denis@gmail.com>
2024-05-20 14:39:10 +0300

    backup

--- overlays/stable.nix
+++ overlays/stable.nix
@@ -1,10 +1,4 @@
 channels: [
   (final: prev: {
-    inherit (channels.nixpkgs-unstable) helm-ls eza bun input-leap git-credential-manager;
-    vimPlugins =
-      prev.vimPlugins
-      // {
-        inherit (channels.nixpkgs-unstable.vimPlugins) vim-helm indent-blankline-nvim;
-      };
   })
 ]
